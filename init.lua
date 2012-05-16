861f7dbaf47a0a6492b146793884d46e4b9edd1e
Marco Scoffier <github@metm.org>
2012-05-16 14:51:35 -0400

    changed return of videoForward to match rest of torch, 2nd arg now returns 0 at EOF or other problems.  See code for video_testme.

--- init.lua
+++ init.lua
@@ -957,16 +957,18 @@ opencv.videoForward =
       local args, videoid, tensor  = dok.unpack(
          {...},
          'opencv.videoForward',
-         [[ Grabs next frame from an open video stream (returns 1 at EOF)]],
+         [[ Grabs next frame from an open video stream (returns 0 at EOF)]],
          {arg='videoid', type='int',
           help='id of video file (can have multiple open video files)',
           req=true},
          {arg='tensor', type='torch.Tensor',
-          help='tensor in which you want to store the frame',
-          req=true}
+          help='tensor in which you want to store the frame'}
       )
-
-      return tensor.libopencv.videoGetFrame(videoid,tensor)
+      if not tensor then
+         tensor=torch.Tensor()
+      end
+      local ret = tensor.libopencv.videoGetFrame(videoid,tensor)
+      return tensor, ret
    end
 
 opencv.videoWriteFrame = 
@@ -1032,22 +1034,27 @@ opencv.video_testme =
         local vid = opencv.videoLoadFile(fname)
         print("Opened " .. fname)
         print(" Video id     : " .. vid )
-        local img = torch.Tensor()
         local fps = opencv.videoGetFPS(vid)
         print(" FPS          : " .. fps)
         local msec = opencv.videoSeek(vid,seekto)
         print(" Seek request : " .. seekto .. "s")
         print(" Seeked to    : " .. msec*0.001 .."s")
         print(" Playing for  : " .. duration .. "s")
-        opencv.videoForward(vid,img)
+        local img,ret = opencv.videoForward(vid)           
         local win = image.display{image=img, win=win}
         for i = 1,duration*fps do
-           opencv.videoForward(vid,img)
-           win = image.display{image=img, win=win}
-           if (i == 1) then
-              print(" 1st frame at : " ..
-                    opencv.videoGetMSEC(vid)*0.001 .."s")
+           sys.tic()
+           img, ret = opencv.videoForward(vid,img)
+           if (ret<1) then
+              break;
+           else
+              win = image.display{image=img, win=win}
+              if (i == 1) then
+                 print(" 1st frame at : " ..
+                       opencv.videoGetMSEC(vid)*0.001 .."s")
+              end
            end
+           print("Frame[",i,"] FPS: ", 1/sys.toc())
         end
         print(" Closing id   : " .. vid)
         opencv.videoCloseFile(vid)
